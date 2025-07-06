9f2bd7eb70fa383bbd18dd9beb3319c891018c8f
Septian Adi Pratama <septianadip7998@gmail.com>
2025-07-06 22:46:51 +0700

    components added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,6 @@ export default {
   theme: {
     extend: {
       colors: {
-        // “ungu Ukuiki” – ganti kalau Bos suka shade lain
         primary: {
           DEFAULT: "#7e22ce",   // 700
           light:  "#a855f7",    // 500
