768e0e9bfe67904eb50dfa1f84b8bbd89d910d95
Septian Adi Pratama <septianadip7998@gmail.com>
2025-07-10 19:15:36 +0700

    link desc edited

--- src/data/events.js
+++ src/data/events.js
@@ -6,7 +6,7 @@ export default [
     time: "09.30 AM – 11.00 AM",
     location: "Jakarta Escape, Jatimurni, Jakarta",
     desc: "A beginner class for those who want to learn basic ukuleles. You can borrow ukuleles from us. (Limited)",
-    link: "https://wa.me/62818749604?text=Saya%20mau%20ikut%20workshop%20pemula",
+    link: "https://wa.me/62818749604?text=Saya%20mau%20ikut%20workshop%20Summer%20Class%20UKUiki",
     photo: "/banner/summer-class.webp",
   },
 ];
