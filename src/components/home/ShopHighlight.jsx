8f72d49c82460e2be9487954cf80c907304340ec
Septian Adi Pratama <septianadip7998@gmail.com>
2025-07-15 14:51:15 +0700

    whatsapp linked

--- src/components/home/ShopHighlight.jsx
+++ src/components/home/ShopHighlight.jsx
@@ -3,7 +3,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination } from "swiper/modules";
 import { Link } from "react-router-dom";
 import { FiShoppingCart, FiShoppingBag } from "react-icons/fi";
-import { buyProduct } from "../utils/BuyProduct.jsx";
+import { BuyProduct } from "../utils/BuyProduct.jsx";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
@@ -44,7 +44,7 @@ function ProductCardMini({ product }) {
             {product.price}
           </span>
           <a
-            href={buyProduct(product.name)}
+            href={BuyProduct(product.name)}
             target="_blank"
             rel="noopener noreferrer"
             className={`inline-flex items-center gap-2 rounded-full px-4 py-2 text-sm shadow transition font-medium ${
