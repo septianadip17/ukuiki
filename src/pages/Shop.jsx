1d0b360d7e953c85177fc37e9110c981bc85d2f4
Septian Adi Pratama <septianadip7998@gmail.com>
2025-07-16 15:46:41 +0700

    cols fix at shop for mobile

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -32,7 +32,7 @@ export default function Shop() {
       </div>
 
       {/* Product Grid */}
-      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+      <div className="grid gap-6 grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {filteredProducts.length > 0 ? (
           filteredProducts.map((product) => (
             <ProductCard
