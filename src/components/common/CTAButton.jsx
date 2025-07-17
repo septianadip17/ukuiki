98ffe886128964a70e47c0fe29d4aaf877b3214a
Septian Adi Pratama <septianadip7998@gmail.com>
2025-07-17 20:00:45 +0700

    cta button

---
+++ src/components/common/CTAButton.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+export default function CTAButton({ to, icon: Icon, label }) {
+  return (
+    <div className="p-4 border-b border-gray-200 text-center">
+      <Link
+        to={to}
+        className="group inline-flex justify-center items-center gap-3 rounded-full bg-primary mb-4 px-6 py-3 text-white font-medium shadow-md hover:bg-primary-dark transition duration-300"
+      >
+        <Icon className="text-xl group-hover:animate-bounce" />
+        <span>{label}</span>
+      </Link>
+    </div>
+  );
+}
