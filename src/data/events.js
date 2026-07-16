19028b0d6ee692ed9fabfa9e2bf5defda96b97ba
Septian Adi Pratama <septianadip7998@gmail.com>
2026-07-17 03:50:30 +0700

    added 2 new banner

--- src/data/events.js
+++ src/data/events.js
@@ -23,9 +23,28 @@ export default [
     title: "UKUiki Jamu Time",
     date: "2026-01-31",
     time: "10.30 AM",
-    location: "At at Suwe Ora Jamu di Dwangsa9 - Dharmawangsa Square Jaksel",
+    location: "At Suwe Ora Jamu di Dwangsa9 - Dharmawangsa Square Jaksel",
     desc: `Don't miss it! Our first #ukuleleworkshop in 2026 🥰 Learning how to play ukulele while sipping Jamu and meet new friends! 🎵
           IDR 140K/pax including snack & jamu tasting.`,
     photo: "/banner/jamu-time2.png",
+  },
+  {
+    id: 4,
+    title: "UKUiki Summer Class",
+    date: "2026-07-24",
+    time: "05.00 PM - Finish",
+    location:
+      "At @klikhub.space | Jl. Mahendradatta Selatan No.57, Padangsambian, Denpasar Barat",
+    desc: `Bali! Let's learn to play the #ukulele together! 🌺🎶 Join our class this summer and have fun 🫶 earning UKULELE | meet new friends | fun. IDR 110K/pax Include all learning material & drink You can borrow ukulele from us.`,
+    photo: "/banner/ukuiki_klikhubspace.png",
+  },
+  {
+    id: 5,
+    title: "UKUiki Summer Class",
+    date: "2026-07-19",
+    time: "10.00 AM",
+    location: "at @workunusual.jkt | Lebak Bulus, Cilandak Jaksel",
+    desc: `Jaksel! Udah siap beloom? We're back! Let's learn to strum #ukulele this summer 🌺🎶 Join our class this summer and have fun 🫶 earning UKULELE | meet new friends | fun. IDR 150K | Include drink you can borrow ukulele from us  `,
+    photo: "/banner/ukuiki_klikhubspace.png",
   },
 ];
